ac4eb235524b8fe6f626a8471f10780be7edea57
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-11-28 11:46:14 -0300

    feat: create data query service

--- query/index.ts
+++ query/index.ts
@@ -7,12 +7,46 @@ const app: Express = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+interface Comment {
+  id: string;
+  content: string;
+}
+
+interface PostWithComments {
+  id: string;
+  title: string;
+  comments: Comment[];
+}
+
+interface PostsCollection {
+  [key: string]: PostWithComments;
+}
+
+const postsCollection: PostsCollection = {} as PostsCollection;
+
 app.get('/posts', (req: Request, res: Response) => {
   res.send({posts : []});
 });
 
 app.post('/events', (req: Request, res: Response) => {
-  console.log('Received Event', req.body.type);
+  const { type, data } = req.body;
+
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    postsCollection[id] = {
+      id,
+      title,
+      comments: [],
+    };
+  }
+
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const post = postsCollection[postId];
+    post.comments.push({ id, content });
+  }
+
+  console.log(postsCollection)
 
   res.send({});
 });
