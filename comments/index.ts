566b4f445b75a1d67897a45ae034af5d24243166
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-11-29 12:02:37 -0300

    feat(comment-service): save updated event when comment is moderated and dispatches it as CommentUpdated

--- comments/index.ts
+++ comments/index.ts
@@ -54,6 +54,31 @@ app.post('/posts/:id/comments', async (req: Request, res: Response) => {
 app.post('/events', (req: Request, res: Response) => {
   console.log('Received Event', req.body.type);
 
+  const { type, data } = req.body;
+
+  if (type === 'CommentModerated') {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => comment.id === id);
+
+    if (comment) 
+      comment.status = status;
+
+    axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    }).catch((err) => {
+      console.log(err.message);
+    });
+  }
+
+
   res.send({});
 });
 
