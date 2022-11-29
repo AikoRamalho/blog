a468c8b780b667c09a9cdaba9c962b6065a6298f
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-11-29 11:50:01 -0300

    feat(moderation-service): create commentcreated event handler

--- moderation/index.ts
+++ moderation/index.ts
@@ -5,7 +5,32 @@ import axios from 'axios';
 const app: Express = express();
 app.use(bodyParser.json());
 
+function isApproved(content: string): boolean {
+  const isContentApproved = !content.includes('orange');
+
+  return isContentApproved;
+}
+
+
 app.post('/events', async (req: Request, res: Response) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = isApproved(data.content) ? 'approved' : 'rejected';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    }).catch((err) => {
+      console.log(err.message);
+    });
+  }
+
   res.send({})
 });
 
