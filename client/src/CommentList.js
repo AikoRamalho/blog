c7f687929b053bf9df19c48dd115aefd3cc58814
Aiko Ramalho <aiko.ramalho@gmail.com>
2022-11-29 12:27:30 -0300

    feat(client): create render condition depending on comments status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,6 +2,15 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const renderedComments = comments.map((comment) => {
+    if (comment.status === "approved") {
+      return <li key={comment.id}>{comment.content}</li>;
+    }
+    if (comment.status === "pending") {
+      return <li key={comment.id}>This comment is awaiting moderation</li>;
+    }
+    if (comment.status === "rejected") {
+      return <li key={comment.id}>This comment has been rejected</li>;
+    }
     return <li key={comment.id}>{comment.content}</li>;
   });
 
